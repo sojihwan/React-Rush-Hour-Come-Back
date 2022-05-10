fe190f99deeb45f17cd0da8a6dd1915b5d320b4c
sojihwan <73238543+sojihwan@users.noreply.github.com>
2022-05-10 15:28:23 +0900

    5/10

---
+++ rush-houp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './REACT-RUSH-HOUR-HOME/RUSH-HOUR-HOME/Body'
+import Body from './REACT-RUSH-HOUR-HOME/RUSH-HOUR-HOME/Body';
+
+function App() {
+  return (
+    <div>
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ rush-houp/src/REACT-RUSH-HOUR-HOME/RUSH-HOUR-HOME/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./HOME-CSS/Body.css";
+function Body(){
+    return(
+        <div className='body'>
+            <div className='World'></div>
+            <div className='World'></div>
+            <div className='World'></div>
+            <div className='World'></div>
+        </div>
+    );
+}
+
+export default Body
